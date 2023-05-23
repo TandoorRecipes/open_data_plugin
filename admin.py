9fd6f357a97880120210a3b08cfdab4991696eb1
vabene1111 <vabene1234@googlemail.com>
2023-05-23 14:25:56 +0200

    added od user admin

--- admin.py
+++ admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import OpenDataUnit, OpenDataCategory, OpenDataStore, OpenDataFoodProperty, OpenDataFood, OpenDataConversion, OpenDataProperty
+from .models import OpenDataUnit, OpenDataCategory, OpenDataStore, OpenDataFoodProperty, OpenDataFood, OpenDataConversion, OpenDataProperty, OpenDataUser
 
 
 # Register your models here.
+
+class OpenDataUserAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
+    list_filter = ('moderator_user', 'verified_user',)
+    list_display = ('name', 'moderator_user', 'verified_user',)
+
+    @staticmethod
+    def name(obj):
+        return obj.user.username
+
+
+admin.site.register(OpenDataUser, OpenDataUserAdmin)
+
+
 class OpenDataUnitAdmin(admin.ModelAdmin):
     list_display = ('slug', 'name', 'updated_at', 'created_by',)
 
