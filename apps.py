30b062237345fa7c1a4717f2144baeaa1911b6e1
vabene1111 <vabene1234@googlemail.com>
2025-08-21 18:25:06 +0200

    cleanup

--- apps.py
+++ apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class OpenDataConfig(AppConfig):
-    VERSION = '0.2.0'
     website = 'https://github.com/TandoorRecipes/open_data_plugin'
     github = 'https://github.com/TandoorRecipes/open_data_plugin'
 
@@ -10,12 +9,7 @@ class OpenDataConfig(AppConfig):
     name = 'recipes.plugins.open_data_plugin'
     verbose_name = 'Tandoor Open Data'
     base_url = 'open-data/'
-    bundle_name = 'OPEN-DATA'
     # name of DRF router in urls.py that should be included by the base router (make sure to give all objects a plugin prefix to avoid collisions)
     api_router_name = 'open_data_router'
-    # name of template to include in the main nav section
-    nav_main = ''  # not implemented yet
-    # name of template to include in the dropdown nav section
-    nav_dropdown = 'open_data_dropdown_nav.html'
 
     disabled = False
