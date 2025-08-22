b0b2eca36658b254908969bd7897b73e387abcb6
vabene1111 <vabene1234@googlemail.com>
2025-08-22 16:28:14 +0200

    fixed path creation

--- setup_repo.py
+++ setup_repo.py
@@ -10,6 +10,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.pa
 VUE_BASE_PATH = os.path.join(BASE_DIR, 'vue3', 'src', 'plugins')
 PLUGIN_VUE_BASE_PATH = os.path.join(BASE_DIR, 'recipes', 'plugins', 'open_data_plugin', 'frontend')
 
+os.makedirs(VUE_BASE_PATH, exist_ok=True)
+
 print(f'LINKING FROM {VUE_BASE_PATH} to {PLUGIN_VUE_BASE_PATH}')
 links = [
     [PLUGIN_VUE_BASE_PATH, os.path.join(VUE_BASE_PATH, 'open_data_plugin')],
